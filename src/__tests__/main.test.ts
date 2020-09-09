8a81d926c33ce2ecccbb4226e9e19db81e1a6595
Marko Ristin <marko.ristin@gmail.com>
2020-09-09 11:04:26 +0200

    Innstruct how to improve failing messages
    In many teams, the wider team is not aware how the GitHub action has
    been set up and merely sees the messages in their CI reports. These
    users are ultimately lost and need further guidance on how to fix the
    errors in their commit messages. In particular, sparse error messages
    with no hints how to remedy the commit messages are almost useless
    leaving the users utterly frustrated.
    
    This change introduces very verbose error messages which include the
    hints what can be done about the message. While the messages are now
    quite long and seem cluttered, they are hopefully much more useful to
    the inexperienced users.

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -87,7 +87,7 @@ it('formats properly no error message.', () => {
 
 it('formats properly errors on a single message.', () => {
   (commitMessages.retrieve as any).mockImplementation(() => [
-    'SomeClass to OtherClass\n\nSomeClass with OtherClass'
+    'change SomeClass to OtherClass\n\nSomeClass with OtherClass'
   ]);
 
   const mockSetFailed = jest.fn();
@@ -97,9 +97,11 @@ it('formats properly errors on a single message.', () => {
   expect(mockSetFailed.mock.calls).toEqual([
     [
       'The message 1 is invalid:\n' +
-        '* The subject must start with a capitalized verb (e.g., "Change").\n' +
+        '* The subject must start with a capitalized word, but ' +
+        'the current first word is: "change". ' +
+        'Please capitalize to: "Change".\n' +
         'The original message was:\n' +
-        'SomeClass to OtherClass\n' +
+        'change SomeClass to OtherClass\n' +
         '\n' +
         'SomeClass with OtherClass\n'
     ]
@@ -108,7 +110,7 @@ it('formats properly errors on a single message.', () => {
 
 it('formats properly errors on two messages.', () => {
   (commitMessages.retrieve as any).mockImplementation(() => [
-    `SomeClass to OtherClass\n\n${'A'.repeat(73)}`,
+    `change SomeClass to OtherClass\n\nDo something`,
     'Change other subject\n\nChange body'
   ]);
 
@@ -119,23 +121,21 @@ it('formats properly errors on two messages.', () => {
 
   expect(mockSetFailed.mock.calls).toEqual([
     [
-      `${'The message 1 is invalid:\n' +
-        '* The subject must start with a capitalized verb (e.g., "Change").\n' +
-        '* The line 3 of the message (line 1 of the body) exceeds ' +
-        'the limit of 72 characters. The line contains 73 characters: "'}${'A'.repeat(
-        73
-      )}".\n` +
-        `The original message was:\n` +
-        `SomeClass to OtherClass\n` +
-        `\n${'A'.repeat(73)}\n` +
-        `\n` +
-        `The message 2 is invalid:\n` +
-        `* The first word of the subject ("Change") must not match ` +
-        `the first word of the body.\n` +
-        `The original message was:\n` +
-        `Change other subject\n` +
-        `\n` +
-        `Change body\n`
+      'The message 1 is invalid:\n' +
+        '* The subject must start with a capitalized word, ' +
+        'but the current first word is: "change". ' +
+        'Please capitalize to: "Change".\n' +
+        'The original message was:\n' +
+        'change SomeClass to OtherClass\n\nDo something\n\n' +
+        'The message 2 is invalid:\n' +
+        '* The first word of the subject ("Change") must not match ' +
+        'the first word of the body. Please make the body more informative ' +
+        'by adding more information instead of repeating the subject. ' +
+        'For example, start by explaining the problem that this change ' +
+        'is intended to solve or what was previously missing ' +
+        '(e.g., "Previously, ....").\n' +
+        'The original message was:\n' +
+        'Change other subject\n\nChange body\n'
     ]
   ]);
 });
